6a686c1f0968eb213461c2003171324d640d93f0
pianomister <stegiemail-github@yahoo.de>
2015-03-13 10:24:13 +0100

    Firefox app bug fix

--- plugins/links.js
+++ plugins/links.js
@@ -2,7 +2,8 @@ plugins.push(new Linkbox());
 
 function Linkbox() {
 	
-	var manifest_url = location.href + 'manifest.webapp';
+	//Firefox manifest URL
+	var manifest_url = 'http://topahl.github.io/DHBW-Dashboard/manifest.webapp';
 	
 	function checkFirefox() {
 		return (typeof navigator !== 'undefined' && typeof navigator.mozApps !== 'undefined');
@@ -10,16 +11,15 @@ function Linkbox() {
 	
 	function install(ev) {
 		ev.preventDefault();
-		console.debug("test");
-		//Manifest URL Definieren
-		// App Installieren
+
+		// install app
 		var installLocFind = navigator.mozApps.install(manifest_url);
 		installLocFind.onsuccess = function(data) {
-			// Wenn die App Installiert ist
+			// app install successful
 			alert('DHBW Dashboard wurde installiert!');
 		};
 		installLocFind.onerror = function() {
-			// App ist nicht Installiert
+			// app install not successful
 			// installapp.error.name
 			alert('Fehler bei der Installation: \n' + installLocFind.error.name);
 		};
@@ -38,7 +38,7 @@ function Linkbox() {
 				if(installCheck.result) {
 					$button.hide();
 				} else {
-					$button.on('click', function() { install(); });
+					$button.on('click', function(e) { install(e); });
 				};
 			};
 		} else {
@@ -50,41 +50,23 @@ function Linkbox() {
 		
 		var html ='<div class="list-box"><ul>'+
 					'<li><a class="item-icon-wrapper" id="vorlesungsplan-link" href="http://vorlesungsplan.dhbw-mannheim.de/index.php" target="_blank">'+
-						'<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">'+
-							'<path fill="#999999" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />'+
-						'</svg></span>'+
-						'<div>Vorlesungsplan aufrufen</div>'+
+					'<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">'+
+					'<path fill="#999999" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />'+
+					'</svg></span>'+
+					'<div>Vorlesungsplan aufrufen</div>'+
 					'</a></li>';
 		
 		if(checkFirefox()) {
 			html +=	'<li id="button-install-firefox" class="item-icon-wrapper link">'+
-						'<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">'+
-								'<path fill="#999999" d="M16,20H20V16H16M16,14H20V10H16M10,8H14V4H10M16,8H20V4H16M10,14H14V10H10M4,14H8V10H4M4,20H8V16H4M10,20H14V16H10M4,8H8V4H4V8Z" />'+
-						'</svg></span>'+
-						'<div>Als Firefox-App installieren</div>'+
+					'<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">'+
+					'<path fill="#999999" d="M16,20H20V16H16M16,14H20V10H16M10,8H14V4H10M16,8H20V4H16M10,14H14V10H10M4,14H8V10H4M4,20H8V16H4M10,20H14V16H10M4,8H8V4H4V8Z" />'+
+					'</svg></span>'+
+					'<div>Als Firefox-App installieren</div>'+
 					'</li>';
-			}
+		}
 		
 		html += '</ul></div>';
-
-/*					<!--TODO TESTING ONLY-->
-					<ul>
-						<li><a class="item-icon-wrapper" id="vorlesungsplan-link" href="http://vorlesungsplan.dhbw-mannheim.de/" target="_blank">
-
-						<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-							<path fill="#a1887f" d="M2,21H20V19H2M20,8H18V5H20M20,3H4V13A4,4 0 0,0 8,17H14A4,4 0 0,0 18,13V10H20A2,2 0 0,0 22,8V5C22,3.89 21.1,3 20,3Z" />
-						</svg></span>
-							<div>
-						Kaffeepause
-								</div>
-					</a></li>
-						<li><span class="item-icon-wrapper">
-						<span class="item-icon"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-							<path fill="#ffd54f" d="M14.4,6L14,4H5V21H7V14H12.6L13,16H20V6H14.4Z" />
-						</svg></span>
-						<div>Flagge hissen</div>
-					</span></li>
-					</ul>*/
+		
 		$(object).html(html);
 	}
 	
